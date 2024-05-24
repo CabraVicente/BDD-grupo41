d419a3bcc94edeb6722c9fef5d6c62713da94522
Sebastian Perez <seba.perez.carvajal@gmail.com>
2024-05-24 19:02:17 -0400

    avance en consultas 4 y 5

--- cgi-bin/consultas.py
+++ cgi-bin/consultas.py
@@ -16,7 +16,7 @@ try:
         port=5432
     )
 except:
-    print("NO HAY CONNEXXXX")
+    print("NO HAY CONEXION")
 
 cur = conn.cursor()
 
@@ -25,6 +25,12 @@ param = [
     form.getvalue("param0"),
     form.getvalue("param1"),
     form.getvalue("param2")
+]
+
+param = [
+    param[0] if param[0] is not None else "",
+    param[1] if param[1] is not None else "",
+    param[2] if param[2] is not None else "",
 ]
 consulta = form.getvalue("type")
 
@@ -35,7 +41,7 @@ def result_get(cursor):
     return {"nombres":[d[0] for d in cur.description], "datos":cur.fetchall()}
 
 if consulta == 0:
-    if (param[2] != None):
+    if (param[2] != ""):
         cur.execute(
             """SELECT %s FROM %s WHERE %s""" %
             (param[0], param[1], param[2])
@@ -48,10 +54,30 @@ if consulta == 0:
     resultado = result_get(cur)
 elif consulta == 1:
     cur.execute(
-        """SELECT restaurante.nombre FROM restaurante,plato
-        WHERE plato.restaurante_nombre = restaurante.nombre AND plato.nombre=%s AND plato.disponibilidad=True""",
+        """SELECT restaurante.nombre AS Restaurante FROM restaurante,plato
+        WHERE plato.restaurante_nombre = restaurante.nombre AND plato.nombre=%s AND plato.disponibilidad=True
+        ORDER BY restaurante.nombre""",
         (param[0],)
     )
+    resultado = result_get(cur)
+elif consulta == 4 or consulta == 5:
+    if (param[1] != "on"):
+        cur.execute(
+            """SELECT nombre, restriccion
+            FROM plato
+            WHERE plato.estilo=%s
+            ORDER BY nombre""",
+            (param[0],)
+        )
+    else:
+        cur.execute(
+            """SELECT plato.nombre AS plato, restaurante.nombre AS restaurante
+            FROM plato, restaurante
+            WHERE plato.restaurante_nombre = restaurante.nombre AND
+                plato.estilo=%s
+            ORDER BY plato.nombre""",
+            (param[0],)
+        )
     resultado = result_get(cur)
 
 print_table(resultado)
