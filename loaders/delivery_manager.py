e06388c3aff40d9e908133729459e710a6935c4b
CabraVicente <vicente.cabra@uc.cl>
2024-05-24 02:36:24 -0400

    delivery manager fiexeao

--- loaders/delivery_manager.py
+++ loaders/delivery_manager.py
@@ -1,24 +1,13 @@
 import psycopg2 as psql
-import os
+import loader_tools as loader
 
-conexion_insana = psql.connect(
-    database="grupo41e2",
-    user="grupo41",
-    host="pavlov.ing.puc.cl",
-    password="2_balas",
-    port=5432
-)
+conn = loader.connect()
+cur = conn.cursor()
 
-cur = conexion_insana.cursor()
+cur.execute("DROP TABLE IF EXISTS DELIVERY_MANAGER CASCADE;")
 
-cur.execute("DROP TABLE IF EXISTS DELIVERY_MANAGER;")
 
-script_dir = os.path.dirname(__file__)
-csv_path = "./data/cldeldes.csv"
-
-with open(csv_path, "r") as texto:
-    deliveries = texto.readlines()
-headers = deliveries[0].strip().split(";")
+data = loader.load_table("./data/cldeldes.csv")
 
 cur.execute(
     """CREATE TABLE DELIVERY_MANAGER(
@@ -26,21 +15,16 @@ cur.execute(
     vigente BOOLEAN
     );"""
 )
-conexion_insana.commit()
 
-for linea in deliveries[1:]:
-    if (linea.count(";") < 5 or linea.count('"') % 2 == 1):
-        print("ERROR en linea: "+linea)
-        continue
-
-    clientenombre, clienteemail, clientetelefono, clienteclave, deliverynombre, deliveryvigente, deliverytelefono, deliverytiempo, deliverypreciounitario, deliverypreciomensual, deliveryprecioanual, despachadornombre, despachadortelefono = linea.strip().split(";")
-
+for fila in data["datos"]:
+    clientenombre, clienteemail, clientetelefono, clienteclave, deliverynombre, deliveryvigente, deliverytelefono, deliverytiempo, deliverypreciounitario, deliverypreciomensual, deliveryprecioanual, despachadornombre, despachadortelefono = fila
     cur.execute(
         "INSERT INTO DELIVERY_MANAGER(nombre, vigente) VALUES (%s, %s) ON CONFLICT (nombre) DO NOTHING",
-        (deliverynombre, bool(deliveryvigente))
+        (deliverynombre, True if deliveryvigente=="TRUE" else False)
     )
+    conn.commit()
 # En caso de que el loader no estÃ© del todo completo, aÃ±adir crear tabla 'CLIENTE' y dropear 'CLIENTES'
 
-conexion_insana.commit()
+conn.commit()
 cur.close()
-conexion_insana.close()
+conn.close()
